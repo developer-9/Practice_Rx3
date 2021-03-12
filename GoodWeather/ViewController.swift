50523560c1d795f4b6b076014f4e9d6f01de95cb
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-03-13 00:01:58 +0900

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Taisei Sakamoto on 2021/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
